6bd28b8209f91574512364bfe541f4c6e8f3fb93
Tritintruong114 <truongtritin.bee@gmail.com>
2023-04-22 10:43:40 +0700

    This is for menu

--- my-bruno/src/components/TopNav.jsx
+++ my-bruno/src/components/TopNav.jsx
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import {
+  UilSearch,
+  UilBell,
+  UilBars,
+  UilMultiply,
+} from "@iconscout/react-unicons";
 function TopNav() {
+  const [clickMenu, setClickMenu] = useState(true);
+
+  const handleClick = () => {
+    setClickMenu((prev) => !prev);
+    console.log("Click");
+  };
   return (
     <div className="w-full flex items-center justify-center gap-3">
-      <Link to={"home"}>
-        <button>Home</button>
-      </Link>
-      <Link to={"about"}>
-        <button>About</button>
-      </Link>
-      <Link to={"projects"}>
-        <button>Projects</button>
-      </Link>
+      <div>
+        <h1>Bruno</h1>
+      </div>
+      <div
+        className={`flex ${
+          !clickMenu ? "opacity-100" : "opacity-0"
+        } flex-col items-center absolute right-3 top-3 bg-white w-full`}
+      >
+        <Link to={"home"}>
+          <button>Home</button>
+        </Link>
+        <Link to={"about"}>
+          <button>About</button>
+        </Link>
+        <Link to={"projects"}>
+          <button>Works</button>
+        </Link>
+        <Link to={"projects"}>
+          <button>Blog</button>
+        </Link>
+        <Link to={"projects"}>
+          <button>Contact</button>
+        </Link>
+      </div>
+      {clickMenu ? (
+        <div className="absolute right-6 top-4" onClick={() => handleClick()}>
+          <UilBars />
+        </div>
+      ) : (
+        <div className="absolute right-6 top-4" onClick={() => handleClick()}>
+          <UilMultiply />
+        </div>
+      )}
     </div>
   );
 }
